df6c708a7e9e2b0f180674326f48df058499ca0f
Andrew Michael Koelling <akoellin@bb136-01.mines.edu>
2014-12-02 17:26:04 -0700

    I think i got a simple gaussian approximation going

--- mod_lehmer.f95
+++ mod_lehmer.f95
@@ -1,7 +1,8 @@
 MODULE Lehmer
 	IMPLICIT NONE
-	PRIVATE n, g, mod_seed
+	PRIVATE n, g, mod_seed, z1
 	INTEGER*8::n=2147483647, g=48271, mod_seed = 0
+	REAL::z1 = -1.0
 	contains 
 		subroutine seed(x)
 			INTEGER::x
@@ -31,6 +32,21 @@ MODULE Lehmer
 			REAL::randomuni
 			randomuni=uniform()*(b-a)+a
 		end function
-		
+		function gaussApprox() Result(gauss)
+			REAL::u1, u2
+			REAL::pi = 4.*atan(1.)
+			REAL::gauss
+			!check if there is a value from the last pair 
+			if(z1== -1.0) then 
+				u1 = uniform()
+				u2 = uniform()
+				gauss = ((-2*log(u1))**.5) * cos(2*pi*u2)
+				z1 = ((-2*log(u1))**.5) * cos(2*pi*u2)
+			else
+				gauss = z1;
+				z1 = -1.0;
+				!just use the second value from the last genration
+			end if
+		end function
 
 end module 
--- test.f95
+++ test.f95
@@ -3,13 +3,14 @@ program test
 use Lehmer
 implicit none
 INTEGER::x, i
-REAL::uni, expon
+REAL::uni, expon, gauss
 call seed(100)
 DO i =1,10
 	x = randintRange(10, 30)
 	uni=uniformRange(15,18)
 	expon=exponential(2.)
+	gauss = gaussApprox()
 
-	write(*,*) x, uni, expon
+	write(*,*) x, uni, expon, gauss
 end do
 end program 
